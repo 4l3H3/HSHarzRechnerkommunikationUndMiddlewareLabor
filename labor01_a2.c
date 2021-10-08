6e8f8e873768a1ab3ba15322286156c9462d90dd
alehe <alehe98@gmx.de>
2021-10-08 11:06:52 +0200

    changed for loop to true

--- labor01_a2.c
+++ labor01_a2.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
 
 int main(){
   pid_t pid;
-
   printf("Programmstart\n");
   switch (pid = fork()){
     case -1: printf("Fehler bei fork()\n"); break;
     case 0: printf("Ich bin das Kind mit der pid=%d und habe den Vater mit pid=%d\n", getpid(), getppid());
-	    for(int i = 0;i < 9; i++){
-              
-	    }
-	    exit(0);
+	    while(true);
     default: printf("Ich bin der Vater mit pid=%d und habe das Kind mit der pid=%d\n", getpid(), pid);
              wait();
              printf("Kindprozess wurde beendet"); break;
