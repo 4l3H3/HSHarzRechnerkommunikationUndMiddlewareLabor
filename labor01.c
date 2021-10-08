82dd566937f9dad6bf07f07c002f12be7b7ea2a7
alehe <alehe98@gmx.de>
2021-10-08 08:53:09 +0200

    added signal handler function

--- labor01.c
+++ labor01.c
@@ -3,10 +3,19 @@
 #include<signal.h>
 
 void sig_handler(int_signum){
-  printf(int_signum)
+  printf(int_signum);
+  switch(int_signum){
+    case SIGUSR1: printf("Signal SIGUSR1 erhalten")
+    case SIGUSR2: signal(int_signum, SIG_IGN);
+    case SIGINT: printf("ich lasse mich nicht unterbrechen")
+    case SIGALARM: printf("ALARM")
+    case SIGTERM: printf("ich lasse mich nicht terminieren")
+    case SIGKILL: printf("ich lasse mich nicht terminieren")
+    default: printf("Signal nicht erkannt")
+  }
 }
 int main(){
-  signal(SIGINT,SIG_IGN); // Register signal handler for ignoring the signal
+  signal(SIGINT,sig_handler); // Register signal handler for ignoring the signal
 
   for(int i=1;i<10;i++){    //Infinite loop
     printf("%d : Inside main function\n",i);
