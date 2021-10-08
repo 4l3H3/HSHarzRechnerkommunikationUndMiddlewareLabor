e266526b146de4a01ef09ecbac49710b02160b84
alehe <alehe98@gmx.de>
2021-10-08 22:22:47 +0200

    changed if case to switch, added for loop to count, created forkedMemory function

--- labor01_a3.c
+++ labor01_a3.c
@@ -4,14 +4,31 @@
 #include <signal.h>
 #include <stdbool.h>
 
-int main(){
+int ergebnis = 0;
+
+// Diese Funktion soll das Ergbenis über 10 Kindprozesse insgesamt auf 1000000
+// hochzählen. Das Ergebnis am Ende ist 0. Da kein Fehler ausgeworfen wird scheint
+// jedes Kind eine eigene ergebnis Variable zu besitzen und hochzuzählen.
+// Das Ergebnis vom Parent bleibt dabei unverändert.
+int forkedMemory(){
   pid_t pid;
   printf("Programmstart\n");
-  for(int i = 0; i < 10; i++) {
-    if(fork() == 0){
-      printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
-      exit(0);
+  for(int i = 0; i < 10; i++){
+    switch(pid = fork()){
+     case -1: printf("Fehler bei fork()\n"); break;
+     case 0: 
+         printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+         for(int i = 0; i < 9999; i++) ergebnis++;
+         exit(0);
+     default: wait(); break;
     }
   }
+  printf("Ergebnis: %d\n",ergebnis);
   return 0;
 }
+
+int main(){
+  forkedMemory();
+  return 0;
+}
+
