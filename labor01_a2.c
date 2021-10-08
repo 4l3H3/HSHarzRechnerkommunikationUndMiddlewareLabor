c83c26e679d8c65e35e6711484bf6c81565b5a7d
alehe <alehe98@gmx.de>
2021-10-08 10:50:55 +0200

    added pid logic, added breaks to switch, added for loop and exit in child process

--- labor01_a2.c
+++ labor01_a2.c
@@ -4,10 +4,18 @@
 
 int main(){
   pid_t pid;
+
+  printf("Programmstart\n");
   switch (pid = fork()){
-    case -1: printf("Fehler bei fork()\n");
-    case 0: printf("Ich bin das Kind");
-    default: printf("Ich bin der Vater");
+    case -1: printf("Fehler bei fork()\n"); break;
+    case 0: printf("Ich bin das Kind mit der pid=%d und habe den Vater mit pid=%d\n", getpid(), getppid());
+	    for(int i = 0;i < 9; i++){
+              
+	    }
+	    exit(0);
+    default: printf("Ich bin der Vater mit pid=%d und habe das Kind mit der pid=%d\n", getpid(), pid);
+             wait();
+             printf("Kindprozess wurde beendet"); break;
   }
   return 0;
 }
