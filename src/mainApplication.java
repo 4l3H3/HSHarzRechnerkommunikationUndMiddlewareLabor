50843b0984ebdda1d1d284e1f44eab263a3f0a03
alehe <alehe98@gmx.de>
2021-10-09 20:00:39 +0200

    created main method

--- src/mainApplication.java
+++ src/mainApplication.java
@@ -1,2 +1,13 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class mainApplication {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",1234);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
