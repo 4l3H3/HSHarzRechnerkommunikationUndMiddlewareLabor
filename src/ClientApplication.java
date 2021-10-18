4330bca364a56b70060d8d4245352fb2fb604dfa
alehe <alehe98@gmx.de>
2021-10-18 14:04:09 +0200

    implemented Constructor

--- src/ClientApplication.java
+++ src/ClientApplication.java
@@ -1,5 +1,15 @@
 public class ClientApplication {
-    public static void main(String[] args){
 
+    public ClientApplication(int port){
+        connectToServer(port);
+        awaitChallenge();
+        solveChallenge();
+        sendSolvedChallenge();
+        readResponse();
     }
+    public static void main(String[] args){
+        new ClientApplication(8444);
+    }
+
+    
 }
