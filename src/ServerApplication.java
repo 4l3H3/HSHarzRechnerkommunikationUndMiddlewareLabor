1e585d89b5a3f8de43cef77b6b643f1384c484bf
alehe <alehe98@gmx.de>
2021-10-18 13:22:39 +0200

    created createChallenge function, changed parameters of ServerApplication and await Connection

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -1,29 +1,51 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class ServerApplication {
     private int challengeSolution;
     private ServerSocket server;
     private Socket client;
+    private String serverChallenge;
 
-    public ServerApplication(){
-        awaitConnection();
+    public ServerApplication(int port){
+        awaitConnection(port);
+        serverChallenge = createChallenge();
         sendChallenge();
         awaitResponse();
         sendChallengeStatus();
     }
     public static void main(String[] args){
-        new ServerApplication();
+        new ServerApplication(8444);
     }
 
-    private void awaitConnection(){
+    private void awaitConnection(int port){
         try {
-            server = new ServerSocket(8444);
+            server = new ServerSocket(port);
             client = server.accept();
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    private String createChallenge(){
+        char[] operations = new char[] {'+', '-', '*', '/'};
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+
+        //Generate first digit
+        builder.append(random.nextInt(10));
+        //Generate Operation
+        char operation = operations[random.nextInt(4)];
+        builder.append(operation);
+
+        //Generate Last Digit
+        if (operation == '/')
+            builder.append(random.nextInt(1,10));
+        else
+            builder.append(random.nextInt(10));
+        return builder.toString();
     }
 
 
