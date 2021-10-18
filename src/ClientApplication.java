e559f371aaf5c146874e3ff5acb9d1a9cb71bfc6
alehe <alehe98@gmx.de>
2021-10-18 15:18:17 +0200

    implemented sendSolvedChallenge function

--- src/ClientApplication.java
+++ src/ClientApplication.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientApplication {
@@ -50,7 +47,19 @@ public class ClientApplication {
         }
     }
 
-    
+    private void sendSolvedChallenge(){
+        ObjectOutputStream oos;
+        try {
+            serverChallengeInfo.setChallengeSolution(challengeSolution);
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(serverChallengeInfo);
+            oos.flush();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 }
