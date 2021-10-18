6caed6c507295d53025fcd10763c5188192cf33d
alehe <alehe98@gmx.de>
2021-10-18 13:08:30 +0200

    implemented awaitConnection and Constuctor

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApplication {
     private int challengeSolution;
-    public static void main(String[] args){
+    private ServerSocket server;
+    private Socket client;
 
+    public ServerApplication(){
+        awaitConnection();
+        sendChallenge();
+        awaitResponse();
+        sendChallengeStatus();
     }
+    public static void main(String[] args){
+        new ServerApplication();
+    }
+
+    private void awaitConnection(){
+        try {
+            server = new ServerSocket(8444);
+            client = server.accept();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
