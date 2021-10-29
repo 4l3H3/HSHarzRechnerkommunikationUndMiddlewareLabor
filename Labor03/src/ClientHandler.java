f0e574d07bbe20b7e7861c217f891c4569f0b760
alehe <alehe98@gmx.de>
2021-10-29 16:08:34 +0200

    removed Challenge logic

--- Labor03/src/ClientHandler.java
+++ Labor03/src/ClientHandler.java
@@ -1,12 +1,11 @@
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 public class ClientHandler extends Thread{
     private float challengeSolution;
     private Socket socket;
     private MessageInfo messageHandler;
+    private final String WELCOME_MESSAGE = "Hello. I am ELIZA. What is your name?";
 
     public ClientHandler(Socket socket){
         super();
@@ -14,11 +13,9 @@ public class ClientHandler extends Thread{
     }
 
     public void run() {
-        messageHandler = new MessageInfo(createChallenge());
-        challengeSolution = solveChallenge(messageHandler.getChallengeString());
-        sendChallenge();
+        messageHandler = new MessageInfo(WELCOME_MESSAGE);
+        sendWelcomeMessage();
         awaitResponse();
-        sendChallengeStatus();
         try {
             socket.close();
         } catch (IOException e){
@@ -27,39 +24,7 @@ public class ClientHandler extends Thread{
 
     }
 
-    private String createChallenge(){
-        char[] operations = new char[] {'+', '-', '*', '/'};
-        StringBuilder builder = new StringBuilder();
-        Random random = new Random();
-
-        //Generate first digit
-        builder.append(random.nextInt(10));
-        //Generate Operation
-        char operation = operations[random.nextInt(4)];
-        builder.append(operation);
-
-        //Generate Last Digit
-        if (operation == '/')
-            builder.append(random.nextInt(9) + 1);
-        else
-            builder.append(random.nextInt(10));
-        return builder.toString();
-    }
-
-    private float solveChallenge(String serverChallenge){
-        byte[] bytes = serverChallenge.getBytes(StandardCharsets.UTF_8);
-        int first_digit = serverChallenge.charAt(0) - 48;
-        int last_digit = serverChallenge.charAt(2) - 48;
-
-        switch (bytes[1]){
-            case '+': return first_digit + last_digit;
-            case '-': return first_digit - last_digit;
-            case '*': return first_digit * last_digit;
-            case '/': return first_digit / last_digit;
-            default: return Float.NEGATIVE_INFINITY;
-        }
-    }
-    private void sendChallenge(){
+    private void sendWelcomeMessage(){
         ObjectOutputStream oos;
         try {
             oos = new ObjectOutputStream(socket.getOutputStream());
@@ -81,19 +46,4 @@ public class ClientHandler extends Thread{
         }
     }
 
-    private void sendChallengeStatus(){
-        BufferedWriter oos;
-        try {
-            oos = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            if (messageHandler.getChallengeSolution() == challengeSolution) {
-                oos.write("The challenge has been solved. Good Job!");
-            }
-            else {
-                oos.write("Wrong! Try connecting again.");
-            }
-            oos.flush();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 }
